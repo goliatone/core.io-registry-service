90fe772bd1e19adbec54d8ecea3b267e2e5eb4d5
goliatone <burgosemi@gmail.com>
2017-09-10 08:57:07 -0400

    Update api endpoints

--- src/modules/api/routes/endpoints.js
+++ src/modules/api/routes/endpoints.js
@@ -3,15 +3,34 @@
 
 const express = require('express');
 
-module.exports = function(app, config){
-    var router = initialize(app, config);
+/**
+ * Standard entry point for TrackFinder
+ * routes. We are not in the appliction
+ * context scope.
+ *
+ * @method exports
+ * @param  {Object} router Express router
+ * @param  {Object} config
+ * @param  {Object} subapp Express app to
+ *                         be mounted.
+ * @return {void}
+ */
+module.exports = function(router, config, subapp){
 
-    app.use('/', router);
+    initialize(subapp, config);
+
+    router.use('/', subapp);
 };
 
-function initialize(app, config){
-    var router = express.Router();
+function initialize(router, config){
+    // let router = express.Router();
 
+    const context = config.context;
+    const logger = context.getLogger('endpoints');
+
+    const Job = context.models.Job;
+    const Application = context.models.Application;
+    
     /**
      * GET home page.
      */
@@ -20,6 +39,72 @@ function initialize(app, config){
             status: true
         });
     });
+
+    /**
+     * Register a new application instance.
+     * A new application get's an ID which is
+     * sent back as a response. Which should be
+     * used to de-register it.
+     */
+    router.post('/registry/enter', function registrerHandler(req, res){
+        res.send({
+            status: true
+        });
+    });
+
+    router.post('/registry/exit', function registrerHandler(req, res){
+        let appId = req.body.appId;
+        Application.findOne({appId}).then((result)=>{
+            res.send({
+                status: true
+            });
+        });
+    });
+
+    /**
+     * List registered applications.
+     */
+    router.get('/application', function jobHandler(req, res) {
+        Application.find().then((result)=>{
+            res.send({
+                status: true,
+                value: result
+            });
+        });
+    });
+
+    router.get('/application/:id/jobs', function appJobListHandler(req, res){
+        let appId = req.param.id;
+
+        Application.findOne({appId}).populate('jobs').then((result)=>{
+            res.send({
+                status: true,
+                value: result
+            });
+        });
+    });
+
+    router.post('/job', function jobCreateHandler(req, res) {
+        res.send({
+            status: true
+        });
+    });
+
+    router.get('/job', function jobListHandler(req, res){
+        res.send({
+            status: true
+        });
+    });
+
+    router.get('/job/:id', function jobDetailHandler(req, res) {
+        Job.findOne(req.param.id).then((result)=>{
+            res.send({
+                status: true
+            });
+        });
+    });
+
+
 
     return router;
 }
