512ca12b03701383f2b790cd5d59e62daabbcf5a
goliatone <burgosemi@gmail.com>
2017-09-17 18:28:40 -0400

    Handle mqtt request to list apps

--- src/config/pubsub.js
+++ src/config/pubsub.js
@@ -13,6 +13,14 @@ module.exports = {
     },
     handlers: {
         'ww/registry/register': console.log,
-        'ww/registry/unregister': console.log
+        'ww/registry/unregister': console.log,
+        'ww/registry/list': function(topic, event){
+            let context = this;
+            let Application = context.models.Application;
+            Application.find().then((result)=> {
+                console.log('send result to ', event.response, result);
+                context.pubsub.publish(event.response, {result});
+            }).catch(console.error);
+        },
     }
 };
