e645d3822cc9a987ec6cc34b95fbc2597d26ffaf
goliatone <burgosemi@gmail.com>
2017-12-14 17:45:10 -0500

    update confit influx

--- src/config/influxdb.js
+++ src/config/influxdb.js
@@ -7,6 +7,10 @@ module.exports = {
         host: process.env.NODE_INFLUXDB_HOST || '192.168.99.100',
         database: process.env.NODE_INFLUXDB_DB || 'registry'
     },
+    /**
+     * Ideally do not store UUIDs in tags. Consider
+     * moving job id to field. Add app name as tag.
+     */
     schema: [
         {
             measurement: 'probe',
