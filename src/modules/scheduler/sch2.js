310e06a082151f8fab3184ab82483a70b42dc8b6
goliatone <burgosemi@gmail.com>
2018-02-08 20:57:48 -0500

    update scheduler sample

--- src/modules/scheduler/sch2.js
+++ src/modules/scheduler/sch2.js
@@ -5,9 +5,14 @@ const Scheduler = require('./lib/redis');
 const KEY = 'test-key';
 
 const scheduler = new Scheduler({
-    autostart: true,
+    autostart: false,
     host: '192.168.99.100',
     port: 6379
+});
+
+scheduler.start().catch(err => {
+    console.error(err);
+    process.exit(1);
 });
 
 let count = 0;
@@ -19,15 +24,18 @@ let count = 0;
  * was used while scheduling the
  * call.
  */
-scheduler.addHandler({
-    key: KEY,
-    handler,
-    repeat: 3
-}).then((res)=>{
-    console.log('scheduled', res);
-}).catch((err)=>{
-    console.log('err', err);
-});
+scheduler
+    .addHandler({
+        key: KEY,
+        handler,
+        reschedule: true
+    })
+    .then(res => {
+        console.log('scheduled', res);
+    })
+    .catch(err => {
+        console.log('err', err);
+    });
 
 scheduler.addHandler({
     key: KEY,
