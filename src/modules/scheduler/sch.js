f4cdf4bacc89e3c50c3d538541eb873e6aaa560b
goliatone <burgosemi@gmail.com>
2018-02-08 20:57:35 -0500

    update scheduler sample

--- src/modules/scheduler/sch.js
+++ src/modules/scheduler/sch.js
@@ -3,12 +3,18 @@
 const Scheduler = require('./lib/redis');
 
 const KEY = 'test-key';
+
 const expirationTime = 1000;
 
 const scheduler = new Scheduler({
-    autostart: true,
+    autostart: false,
     host: '192.168.99.100',
     port: 6379
+});
+
+scheduler.start().catch((err)=>{
+    console.error(err);
+    process.exit(1);
 });
 
 function eventTriggered(err, task) {
